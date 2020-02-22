ded5f5e5fc176c0b211fda582c79a76303d80a9a
united-bb <arum.o@hotmail.co.jp>
2020-02-22 19:58:04 +0900

    ログイン制御機能を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,11 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required
 
-  def create
+  def index
+    @user = User.new
+  end
+
+  def login
     member = User.find_by(name: params[:name])
     if member&.authenticate(params[:password])
       session[:member_id] = member.id
@@ -9,13 +14,28 @@ class SessionsController < ApplicationController
       redirect_to board_index_path, notice:"ログインしました。"
     else
       flash.alert = "名前とパスワードが一致しません。"
-      render 'logins/index'
+      render 'sessions/index'
     end
   end
 
   def destroy
     session.delete(:member_id)
+    session.delete(:name)
     redirect_to :root
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+
+    if @user.save
+      redirect_to login_index_url, notice: "ユーザを登録しました。"
+    else
+      render 'sessions/new'
+    end
   end
 
 end
